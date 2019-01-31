b40d3e39b095aefe30f9faec2631dca5a887f586
franz sardoma <franz.s@nova-hub.net>
2019-01-31 11:21:43 -0800

    hhmmm C#

---
+++ C#/SeeSharp/SeeSharp/NewClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharp
+{
+    class NewClass
+    {
+        public void printNewClass()
+        {
+            Console.WriteLine("New Class");
+        }
+    }
+}
+
+namespace space
+{
+    class newSpace
+    {
+        public void newNameSpace()
+        {
+            Console.WriteLine("THE F SPACE!");
+        }
+    }
+}
--- C#/SeeSharp/SeeSharp/Program.cs
+++ C#/SeeSharp/SeeSharp/Program.cs
@@ -10,6 +10,46 @@ namespace SeeSharp
     {
         static void Main(string[] args)
         {
+            seeSharp();
+            NewClass testClass = new NewClass();
+            testClass.printNewClass();
+
+            space.newSpace theF = new space.newSpace();
+            theF.newNameSpace();
+
+
+            ObjectTest testOOP = new ObjectTest();
+            testOOP.objectTest1();
+            testOOP.setStringVal("OOP");
+            Console.WriteLine("BREAK");
+            testOOP.printStringVal();
+            Console.ReadLine();
+
+        }
+        static void seeSharp()
+        {
+            Console.Write("SEE SHARP!!");
+        }
+
+       
+        }
+    class ObjectTest
+    {
+        private String testString;
+
+        public void objectTest1()
+        {
+            Console.WriteLine("WTFFF C#");
+        }
+
+        public void setStringVal(String val)
+        {
+            testString = val;
+        }
+        public void printStringVal()
+        {
+            Console.WriteLine(testString);
         }
     }
 }
+    
